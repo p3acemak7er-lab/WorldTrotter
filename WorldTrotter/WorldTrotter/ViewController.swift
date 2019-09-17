1c56295b6a71a3bddcc3c64a6505a55d8c49cf74
MCB <mcbolding@my.milligan.edu>
2019-09-17 08:44:59 -0400

    World Trotter

---
+++ WorldTrotter/WorldTrotter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by Bolding, Michael C on 9/17/19.
+//  Copyright © 2019 Bolding, Michael C. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
